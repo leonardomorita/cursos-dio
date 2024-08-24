cd2506baef6e1b5479c054c8e8947232475b4474
leonardomorita <leonardomorita2@gmail.com>
2024-08-23 23:09:58 -0300

    ✨ Aula: Conversão de Tipos

---
+++ programas/ntt-data-engenharia-de-dados-com-python/01-introducao-ao-ambiente-python-e-projetos-colaborativos/03-conhecendo-a-linguagem-de-programacao-python/3-convertendo_tipos.py
@@ -0,0 +1,10 @@
+print(int(1.12345))
+print(int('16'))
+
+print(float('10.3'))
+print(float(100))
+
+print(type(str(10.3)), str(10.3))
+
+print(95 / 2)
+print(95 // 2) # Só retorna a parte inteira da divisão
