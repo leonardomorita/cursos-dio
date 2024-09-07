41b90603f1ab5940ea109ffd71f3962f15a84d16
leonardomorita <leonardomorita2@gmail.com>
2024-09-06 23:33:49 -0300

    ✨ Aula: Operadores Aritméticos

---
+++ programas/ntt-data-engenharia-de-dados-com-python/02-sintaxe-basica-com-python/01-tipos-de-operadores-com-python/01-operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+preco_produto_1 = 20
+preco_produto_2 = 10
+
+print(preco_produto_1 + preco_produto_2)
+print(preco_produto_1 - preco_produto_2)
+print(preco_produto_1 * preco_produto_2)
+print(preco_produto_1 / preco_produto_2)
+print(preco_produto_1 // preco_produto_2) # Divisão com resultado inteiro
+print(preco_produto_1 ** preco_produto_2) # Exponenciação
+print(preco_produto_1 % preco_produto_2) # Resto da divisão
+
+# Ordem de precedência dos operadores: Parênteses; Expoentes; Multiplicações e Divisões (da esquerda para direita); Somas e Subtrações (da esquerda para direita).
+
+x = 10 + 5 * 4
+print(x)
+x = 10 * (5 + 4)
+print(x)
+x = 10 ** (5 + 4)
+print(x)
